f2fe4d5c8ae93d1c8bcfa5fef7bfbf2fbaf1fbcc
USER <afifasantoso@gmail.com>
2022-11-20 20:14:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/afifawulandariF55121057/FragmentAccount.java
@@ -0,0 +1,22 @@
+package com.example.afifawulandariF55121057;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentAccount extends Fragment {
+    View view;
+    public FragmentAccount() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, Bundle saveInstanceState) {
+        view = inflater.inflate(R.layout.account_fragment, container, false);
+        return view;
+    }
+}
